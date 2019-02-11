3fc4c821f146bd7ac47ab0b3c2692944f05668b0
Charles Bell <CharlesBell@users.noreply.github.com>
2019-02-11 11:31:07 -0600

    Create JPLComet.java

---
+++ JPLComet.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ * @author Charles Bell <cbellh47 at gmail.com>
+ */
+public class JPLComet extends Comet{
+    public String name;
+    public double epochJD;
+    public double perihelionDateJD;
+    public double perihelionDistance;
+    public double eccentricity;
+    public double inclination;
+    public double longitudeOfAscendingNode;
+    public double argumentOfPerihelion;
+
+    public JPLComet(String name, double epochJD, double perihelionDateJD,
+            double perihelionDistance, double eccentricity, double inclination,
+            double longitudeOfAscendingNode, double argumentOfPerihelion) {
+
+        this.name = name;
+        this.epochJD = epochJD;
+        this.perihelionDateJD = perihelionDateJD;
+        this.perihelionDistance = perihelionDistance;
+        this.eccentricity = eccentricity;
+        this.inclination = inclination;
+        this.longitudeOfAscendingNode = longitudeOfAscendingNode;
+        this.argumentOfPerihelion = argumentOfPerihelion;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getEpochJD() {
+        return epochJD;
+    }
+
+    public double getPerihelionDistance() {
+        return perihelionDateJD;
+    }
+    
+    public double getPerihelionJD() {
+        return perihelionDateJD;
+    }
+
+    public double getEccentricity() {
+        return eccentricity;
+    }
+
+    public double geInclination() {
+        return inclination;
+    }
+
+    public double getLongitudeOfAscendingNode() {
+        return longitudeOfAscendingNode;
+    }
+
+    public double getArgumentOfPerihelion() {
+        return argumentOfPerihelion;
+    }
+
+}
